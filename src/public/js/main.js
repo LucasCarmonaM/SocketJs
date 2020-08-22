9e38941ab8db2a5fc8507d98efdcc487caa4fa7f
LucasCM <lucas.carmonam@utem.cl>
2020-08-22 15:52:29 -0400

    beta conexiones y envio de mensajes

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,25 @@
-io();
+$(function (){
+    
+    const socket = io();
+
+    // Obteniendo elementos del dom desde interfaz
+    const $messageForm = $('#message-form');
+    const $messageBox = $('#message');
+    const $chat = $('#chat');
+
+    // Captura de eventos
+    $messageForm.submit(e =>{
+        e.preventDefault();        
+        socket.emit('send message', $messageBox.val());
+        $messageBox.val('');
+        
+    })
+
+    socket.on('new message', mensaje=>{
+        $chat.append(mensaje + '<br/>');
+    })
+
+})
+
+const socket = io();
+
--- src/sockets.js
+++ src/sockets.js
@@ -2,6 +2,11 @@ module.exports = (io) =>{
     
     io.on('connection', (socket) => {
         console.log('a user connected');
+
+        socket.on('send message', mensaje =>{
+            io.sockets.emit('new message', mensaje);    
+        })
+
     });
 }
 
